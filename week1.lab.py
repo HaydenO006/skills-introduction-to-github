cda4bda7057651993e20865fe74c6827d5e63efb
HaydenO006 <26205211@edgehill.ac.uk>
2025-10-07 15:44:18 +0100

    Add files via upload

---
+++ week1.lab.py
@@ -0,0 +1,13 @@
+print("hello world!!!")
+student_name = "Hayden"
+student_age = 19
+print("student Name:",student_name)
+print("student Age:",student_age)
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("the sum of this is:", total)
+
+git add week1_lab.py
+git commit -m "feat; Complete Week 1 lab exercises"
+git push
